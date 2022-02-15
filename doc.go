39e925849b34947a819bd1747f9b10a70990a174
Christian Exenberger <ce@stilgrenze.de>
2022-02-16 00:39:18 +0100

    doc.go updated

--- doc.go
+++ doc.go
@@ -4,7 +4,7 @@ AwesomeLog is a fully compatible drop-in replacement for the standard library lo
 AwesomeLog let you define log levels for each logged messages as well as a PrettyPrint function to print out objects in a pretty readable format.
 It also adds the option to show details of the caller like file path, function name and line number.
 
-Since version v0.11.0 AwesomeLog also provides the functionality to add custom handlers for each LogLevel.
+AwesomeLog also provides the functionality to add custom handlers for each LogLevel.
 
 The simplest way to use AwesomeLog is to just replace the standard library log with AwesomeLog:
 
